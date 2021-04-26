4d66a747997378f204e9c1e771b068ee72cb33c0
Park <C-Thomas.Park@charter.com>
2021-04-26 12:18:02 -0500

    Tweaking

---
+++ Chapter_One.py
@@ -0,0 +1,29 @@
+print("Hello, world!")
+
+print("")
+
+input("Please press any key:")
+
+print("")
+
+reps = input("How many times shall I say 'Hello World'?: ")
+res = int(reps)
+
+for ctr in range (res):
+
+    print ("Hello, world!")
+
+print("")
+
+##Algorithm-- like a plan for how to make the logic to have the computer write "Hello, world!" 20 times in the console/
+##What is your algorithm?
+
+##Now, can you go ahead and make the code for this algorithm?
+
+##Can you try running it, and debug it until it works?
+
+##What kind(s) of errors are you encountering? Syntactic? Semantic? Runtime?
+
+##When the computer performs your task, can you describe what happens inside, roughly? You can consult your Chapter One material to do this.
+
+##Now make the computer say your name 1000 times. Is that difficult for you, or easy?
