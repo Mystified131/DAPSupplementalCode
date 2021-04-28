ff6167907a0d24945a8d0135de9e1d39f1429f22
Park <C-Thomas.Park@charter.com>
2021-04-28 08:12:25 -0500

    Tweaking

---
+++ Chapter_Three.py
@@ -0,0 +1,28 @@
+#Please debug these sections of code, until they work properly
+#Section One
+
+ans1 = input("Please enter one number: ")
+ans2 = input("Please enter a second number: ")
+
+an1 = int(ans1)
+
+sum1 = an1 + ans2
+
+print(sum1)
+
+#Section Two
+
+a = 3
+b = 4.5
+
+print a + b
+
+#Section Three
+
+c = 0
+v = 3.2
+
+print(v/c)
+
+
+
---
+++ Chapter_Threeb.py
@@ -0,0 +1,29 @@
+#Please debug these sections of code, until they work properly
+#Section One
+
+ans1 = input("Please enter one number: ")
+ans2 = input("Please enter a second number: ")
+
+an1 = int(ans1)
+an2 = int(ans2)
+
+sum1 = an1 + an2
+
+print(sum1)
+
+#Section Two
+
+a = 3
+b = 4.5
+
+print(a + b)
+
+#Section Three
+
+c = 1
+v = 3.2
+
+print(v/c)
+
+
+
