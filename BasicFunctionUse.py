7700cb6d975ff7e0e9a85c78a7500a6b0bd550a7
Thomas Park <mystifiedthomas@gmail.com>
2021-05-03 15:19:41 -0500

    Tweaking

---
+++ BasicFunctionUse.py
@@ -0,0 +1,15 @@
+#Here is an example of using functions in Python code
+
+#Here is the function code
+def multiply_by_three(x):
+    y = x * 3
+    return y
+
+#Here is the root code
+v = 3
+z = multiply_by_three(v)
+print("")
+print(z)
+
+if __name__ == "__main__":
+      app.run()
