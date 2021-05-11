7d47d3044a4aa401e3d988306ff7c54f7def74c3
Park <C-Thomas.Park@charter.com>
2021-05-11 13:06:42 -0500

    Tweaking

--- Dictionary.py
+++ Dictionary.py
@@ -1,11 +1,16 @@
+#A dictionary is created using curvy brackets
 adict = {}
 
+#You can add values when you create a dictionary
 bdict = {"one": "red", "two": "blue", "three": "yellow"}
 
+#If you print a dictionary, you will see its key:value pairs
 print(bdict)
 
+#You can call a value by it's key
 print(bdict["one"])
 
+#You can create a dictionary using a loop or other data process
 cdict = {}
 
 for x in range(10):
@@ -13,3 +18,12 @@ for x in range(10):
     cdict[x] = x * 10
 
 print(cdict)
+
+#You can split a dictionary into lists of keys and values
+
+bk = bdict.keys()
+bv = bdict.values()
+
+print(bk)
+
+print(bv)
