3586021437205a6b238b377fa024d26670a5342d
Park <C-Thomas.Park@charter.com>
2021-05-12 18:25:01 -0500

    Tweaking

---
+++ Code_Challenges.py
@@ -0,0 +1,17 @@
+#Create a string, a list, a dictionary
+#1. Declare the data manually
+#2. Use a loop to declare the data
+
+#Create a function that turns 3 strings into a list
+
+#Create a dictionary from 2 lists
+
+#Create a dictionary using loops
+
+#Import a .txt file. Iteratate through the data, creating a list of all strings that start with "S". Print the list.
+
+#Use 3 inputs, send the strings to a function, have the function return a list of the inputs
+
+#Create a code that has 2 functions. Have one call the other as part of the code.
+
+
