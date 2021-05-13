a07ebf738ba47ccd3fc6b8ef24aab9766a523147
Park <C-Thomas.Park@charter.com>
2021-05-13 08:51:59 -0500

    Tweaking

--- Code_Challenges.py
+++ Code_Challenges.py
@@ -2,7 +2,36 @@
 #1. Declare the data manually
 #2. Use a loop to declare the data
 
+astr = "This is a string."
+alst = ["this", "is", "a", "list"]
+adict = { 1 : "red", 2 : "blue", 3 : "yellow"}
+
+lsta = [ 1, 2, 3]
+lstb = ["red", "blue", "yellow"]
+
+dictb = {}
+
+for x in range(3):
+    dictb[lsta[x]] = lstb[x]
+print(dictb)
+
+
 #Create a function that turns 3 strings into a list
+
+def Make_List( a, b, c):
+    alst = []
+    alst.append(a)
+    alst.append(b)
+    alst.append(c)
+    return alst
+
+ast = "one"
+bstr = "two"
+cstr = "three"
+clst = Make_List(ast, bstr, cstr)
+
+print(clst)
+
 
 #Create a dictionary from 2 lists
 
